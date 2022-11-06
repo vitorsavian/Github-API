826606254b35faa744dc1390fba2dcfc4b291cd2
Vitor <vitorandresaviandev@gmail.com>
2022-11-06 17:04:57 -0300

    feat: added adapters for services

---
+++ internal/adapters/services/git/github/service.go
@@ -0,0 +1,18 @@
+package github
+
+import (
+	"net/http"
+	"time"
+)
+
+type GitService struct {
+	httpClient http.Client
+}
+
+func NewGitService() *GitService {
+	return &GitService{
+		httpClient: http.Client{Timeout: time.Second * 30},
+	}
+}
+
+func GetRepositorys()
